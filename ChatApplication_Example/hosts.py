cfbaa8b8f60fe6959c3e770c4a2a57e0ee06caa6
ranni98 <ranna.alemi@student.reutlingen-university.de>
2024-06-24 13:29:32 +0200

    hosts created

--- ChatApplication_Example/hosts.py
+++ ChatApplication_Example/hosts.py
@@ -1,18 +1,23 @@
 import socket
 
+# Initialization of the socket to determine the own IP address
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect(("8.8.8.8", 80))
-my_ip = socket.gethostbyname(socket.gethostname())
+try:
+    sock.connect(("8.8.8.8", 80))
+    my_ip = sock.getsockname()[0]
+finally:
+    sock.close()
 
+# Configuration variables
 buffer_size = 4096
 unicode = 'utf-8'
 multicast_address = '224.0.0.0'
+
+# Initialization of variables
 leader = ''
 neighbour = ''
-
 server_list = []
 client_list = []
-
 is_network_changed = False
 crashed_leader = ''
 crashed_replica = ''
